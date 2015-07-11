66170041588ef28b67c3998075dc88d550230108
Yoonsung <lvev9925@naver.com>
2015-07-11 20:01:27 +0900

    Set Initial Data

--- rchat-server/src/main.rs
+++ rchat-server/src/main.rs
@@ -40,9 +40,12 @@ use std::net::{TcpListener, TcpStream};
 use std::thread;
 use std::collections::HashMap;
 
+extern crate rand;
+use rand::distributions::{IndependentSample, Range};
+
 struct Database {
-    group : HashMap<u64, Group>,
-    user : HashMap<u8, User> 
+    groups : HashMap<u64, Group>,
+    users : HashMap<u8, User> 
 }
 
 struct Group {
@@ -52,7 +55,8 @@ struct Group {
 
 struct User {
     name : String,
-    id : u64
+    id : u64,
+    socket : TcpStream
 }
 
 /*
@@ -61,24 +65,52 @@ fn convert_to_array <'a, T: AsRef<[u8]>>(s: &'a T) -> &'a [u8] {
 }
 */
 
+fn generate_random_user_key() -> u64 {
+    let range = Range::new(u64::min_value(), u64::max_value());
+    let mut rng = rand::thread_rng();
+    
+    range.ind_sample(&mut rng)
+}
+
 fn main() {
+
+    let group = Group {
+        name : "rust_day".to_string(),
+        users : Vec::new()  
+    };
+    
+    let database = Database {
+        groups : HashMap::new(),
+        users : HashMap::new()
+    };
+
     let listener = TcpListener::bind("127.0.0.1:9000").unwrap();
     // accept connections and process them, 64spawning a new thread for each one
     
     loop {
-        for stream in listener.incoming() {
+        for stream in listener.incoming() { 
             println!("incoming!!");
             match stream {
                 Ok(mut stream) => {
+                
+                    let user = User {
+                            name : "test".to_string(),
+                            id : generate_random_user_key(),
+                            socket : stream.try_clone().unwrap()
+                    };
+                    
+                    
+                
                     thread::spawn(move|| {
-                        let mut buffer : [u8; 1024] = [0; 1024];
+                        let mut buffer : [u8; 1024  ] = [0; 1024];
                         stream.read(&mut buffer); // ignore here too
                         
-                        //let prot : Protocol = bincode::decode(&buffer).unwrap();
+                        
                         let prot = to_protocol(&buffer);
                         
                         println!("protocol : {:?}", prot);
                     });
+                    
                 }
                 Err(e) => { /* connection failed */ }
             }
@@ -107,27 +139,4 @@ fn client() {
     let byte_vec = from_protocol(protocol);
     stream.write(&byte_vec);
     stream.flush();
-}
-
-use std::string::String;
-//#[test]
-fn test1() {
-    let string = "foo";
-    println!("{:?}", string.as_bytes());
-}
-
-//#[test]
-fn test2() {
-    let s1 = vec![1,2,3];
-    is_hello(&s1);
-}
-
-fn is_array(arr : String) {
-    print!("Success!");
-}
-
-//fn is_hello<'a, T: AsRef<[u8]>>(s: T) -> &'a [u8] {
-fn is_hello<'a, T: AsRef<[u8]>>(s: &'a T) -> &'a [u8] {
-   //println!("{:?}",s.as_ref());
-   s.as_ref()
 }
